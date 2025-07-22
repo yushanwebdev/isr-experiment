8229270ac104f320d6d85d2199e66858ad5924aa
yushanwebdev <yushanwebdev@gmail.com>
2025-07-22 11:28:53 +0530

    Fix Vercel build by using default .next directory

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: 'build', // Change from '.next' to 'build'
+  /* config options here */
 };
 
 export default nextConfig;
